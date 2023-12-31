b9b3aa30a60bd7bc7ef925f84fa1c1d66c5af890
Filippov <dfilippov@kiev.luxoft.com>
2023-12-31 17:28:26 +0200

    d

--- conftest.py
+++ conftest.py
@@ -4,10 +4,16 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
 
+# driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+# driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+# driver = webdriver.Edge(EdgeChromiumDriverManager().install())
+
+
 @pytest.fixture()
 def setup_driver(request):
     url = request.param
     option = webdriver.ChromeOptions()
+    option.add_argument("--no-sandbox")
     option.add_argument("--headless=new")
     option.add_argument("--disable-gpu")
     driver = webdriver.Chrome(
